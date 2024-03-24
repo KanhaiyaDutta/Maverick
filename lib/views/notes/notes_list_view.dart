4879052c5a86227cfc767bc5f49c4dd279f64e4d
KanhaiyaDutta <duttakanhaiya@gmail.com>
2024-03-25 01:24:44 +0530

    Changed ListView format of Notes_list_view screen to GridView with notes in card widget

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -19,30 +19,79 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: notes.length,
-        itemBuilder: (context, index) {
-          final note = notes.elementAt(index);
-          return ListTile(
-            onTap: () {
-              onTap(note);
-            },
-            title: Text(
-               note.text,
-              maxLines: 1,
-              softWrap: true,
-              overflow: TextOverflow.ellipsis,
+    return GridView.builder(
+      itemCount: notes.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemBuilder: (context, index) {
+        final note = notes.elementAt(index);
+        return Stack(
+          children: [
+            Card(
+              color: Color.fromARGB(255, 220, 215, 215),
+              child: ListTile(
+                shape: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                  color: Colors.black54,
+                  width: 0.7,
+                )),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 6,
+                  vertical: 20,
+                ),
+                onTap: () {
+                  onTap(note);
+                },
+                title: Text(
+                  note.text,
+                  maxLines: 5,
+                  softWrap: true,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
             ),
-            trailing: IconButton(
-              onPressed: () async {
-                final shouldDelete = await showDeleteDialog(context);
-                if (shouldDelete) {
-                  onDeleteNote(note);
-                }
-              },
-              icon: const Icon(Icons.delete),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: IconButton(
+                onPressed: () async {
+                  final shouldDelete = await showDeleteDialog(context);
+                  if (shouldDelete) {
+                    onDeleteNote(note);
+                  }
+                },
+                icon: const Icon(Icons.delete),
+              ),
             ),
-          );
-        });
+          ],
+        );
+      },
+    );
   }
 }
+
+//  itemCount: notes.length,
+//         itemBuilder: (context, index) {
+//           final note = notes.elementAt(index);
+//           return ListTile(
+//             onTap: () {
+//               onTap(note);
+//             },
+//             title: Text(
+//                note.text,
+//               maxLines: 1,
+//               softWrap: true,
+//               overflow: TextOverflow.ellipsis,
+//             ),
+//             trailing: IconButton(
+//               onPressed: () async {
+//                 final shouldDelete = await showDeleteDialog(context);
+//                 if (shouldDelete) {
+//                   onDeleteNote(note);
+//                 }
+//               },
+//               icon: const Icon(Icons.delete),
+//             ),
+//           );
+//         }
